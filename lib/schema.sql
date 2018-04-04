f71f5272d37d1ce84c16771296deb0fedeec2c68
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-04 18:09:11 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -2,7 +2,9 @@ CREATE TABLE characters (
   id INTEGER PRIMARY KEY,
   name TEXT,
   motto TEXT,
-  species TEXT
+  species TEXT,
+  author_id INTEGER,
+  series_id INTEGER
 );
 
 CREATE TABLE books (
