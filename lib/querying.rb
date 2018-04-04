8190c100a4279b39ee881f84c4a3048a997ffc2e
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-04 18:28:47 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.titles, books.year
+  "SELECT books.title, books.year
    FROM books
    WHERE series_id = 1
    ORDER BY books.year ASC"
