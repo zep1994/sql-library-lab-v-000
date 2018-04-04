fe4780bf3194edc2fdb6764a9c351654ff4e07e2
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-04 18:36:04 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,17 +13,32 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species)
+   FROM characters
+   GROUP BY species
+   ORDER BY COUNT(species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT species, COUNT(species)
+   FROM characters
+   GROUP BY species
+   ORDER BY COUNT(species) DESC LIMIT 1"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+   FROM series
+   JOIN characters
+   ON series.author_id = characters.author_id
+   GROUP BY series.title LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(characters.name) as book_count
+   FROM character_books
+   JOIN characters
+   ON character_books.character_id = characters.id
+   GROUP BY characters.name
+   ORDER BY book_count DESC"
 end
